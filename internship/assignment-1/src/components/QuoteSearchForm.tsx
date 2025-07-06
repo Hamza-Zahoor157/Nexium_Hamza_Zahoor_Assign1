c76fa961e5275b1886a2f4e8404ea0bc717fbe4e
Hamza-Zahoor157 <159646669+Hamza-Zahoor157@users.noreply.github.com>
2025-07-06 20:04:14 +0500

    Before Deployment

--- internship/assignment-1/src/components/QuoteSearchForm.tsx
+++ internship/assignment-1/src/components/QuoteSearchForm.tsx
@@ -1,40 +1,7 @@
-// "use client";
-// import { Button } from "@/components/ui/button";
-// import { Input } from "@/components/ui/input";
-// import { useRouter } from "next/navigation";
-
-// export default function QuoteSearchForm({
-//   initialQuery = "",
-// }: {
-//   initialQuery?: string;
-// }) {
-//   const router = useRouter();
-
-//   return (
-//     <form
-//       onSubmit={(e) => {
-//         e.preventDefault();
-//         const formData = new FormData(e.currentTarget as HTMLFormElement);
-//         router.push(`?search=${formData.get("query")}`);
-//       }}
-//       className="flex gap-2 w-full max-w-[600px]"
-//     >
-//       <Input
-//         name="query"
-//         placeholder="Search by topic or author..."
-//         className="flex-1"
-//         defaultValue={initialQuery}
-//       />
-//       <Button type="submit">Search</Button>
-//     </form>
-//   );
-// }
 "use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
-import LoadingSpinner from "@/components/ui/LoadingSpinner";
 
 export default function QuoteSearchForm({
   initialQuery = "",
@@ -42,16 +9,13 @@ export default function QuoteSearchForm({
   initialQuery?: string;
 }) {
   const router = useRouter();
-  const [isLoading, setIsLoading] = useState(false);
 
   return (
     <form
-      onSubmit={async (e) => {
+      onSubmit={(e) => {
         e.preventDefault();
-        setIsLoading(true);
         const formData = new FormData(e.currentTarget as HTMLFormElement);
         router.push(`?search=${formData.get("query")}`);
-        setIsLoading(false);
       }}
       className="flex gap-2 w-full max-w-[600px]"
     >
@@ -61,9 +25,7 @@ export default function QuoteSearchForm({
         className="flex-1"
         defaultValue={initialQuery}
       />
-      <Button type="submit" disabled={isLoading}>
-        {isLoading ? <LoadingSpinner /> : "Search"}
-      </Button>
+      <Button type="submit">Search</Button>
     </form>
   );
 }
